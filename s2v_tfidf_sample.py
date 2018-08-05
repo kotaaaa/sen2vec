c0ecbe106563e5bcc22ae4bf15a89fde68f06e89
kotaaaa <kota.k.1132.pda@gmail.com>
2018-08-05 14:16:41 +0900

    sample, class, etc(without tohoku model)

---
+++ s2v_tfidf_sample.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from gensim.models import KeyedVectors
+import s2v_tfidf
+from scipy import spatial
+
+
+def main():
+
+    corpus=['今日は暑かった．辛いものも食べた．とても汗をかいたけど，楽しかった．','なんて日だ！，これからあんなとこ行くんじゃないわ！二度と行かない！','次元数を操作する必要がある例として配列の転置の例を紹介します．']
+    model_dir = './tohoku_entity_vector/entity_vector/entity_vector.model.bin'
+    model = KeyedVectors.load_word2vec_format(model_dir, binary=True)
+
+    # s2v = s2v_tfidf.s2vtfidf(corpus,model)
+    s2v = s2v_tfidf.s2vtfidf(model)
+
+    wakachi_corpus = s2v.make_wakachi_corpus(corpus)
+    # exit()
+    # print(wakachi_corpus)
+    # exit()
+    vec,X_tfidf = s2v.TFIDF(wakachi_corpus)
+    corpus_vec = s2v.tfidf_sentence_vector(vec,X_tfidf)
+    print('0 and 1',1 - spatial.distance.cosine(corpus_vec[0], corpus_vec[1]))
+    print('0 and 2',1 - spatial.distance.cosine(corpus_vec[0], corpus_vec[2]))
+    print('1 and 2',1 - spatial.distance.cosine(corpus_vec[1], corpus_vec[2]))
+    # print('0 and 3',1 - spatial.distance.cosine(corpus_vec[0], corpus_vec[3]))
+    # print('0 and 4',1 - spatial.distance.cosine(corpus_vec[0], corpus_vec[4]))
+
+
+if __name__ == '__main__':
+    main()
---
+++ tohoku_enti_sample.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from gensim.models import KeyedVectors
+import sys
+model_dir = './tohoku_entity_vector/entity_vector/entity_vector.model.bin'
+model = KeyedVectors.load_word2vec_format(model_dir, binary=True)
+# print(model[u'ニュース'].shape)
+# print(len(model.vocab))
+
+
+# 入力された単語から近い単語をn個表示する
+def similarWords(posi, nega=[], n=10):
+    # print('posi,nega ',posi,nega)
+    cnt = 1 # 表示した単語の個数カウント用
+    # 学習済みモデルからcos距離が最も近い単語n個(topn個)を表示する
+    result = model.most_similar(positive = posi, negative = nega, topn = n)
+    for r in result:
+        print('Top10 Similar',cnt,' ', r[0],' ', r[1])
+        cnt += 1
+
+def main():
+    word = sys.argv[1]
+    # word = unicode(word, 'utf-8')
+    similarWords([word])
+    print('model[word]\n',model[word],len(model[word]))
+
+if __name__ == '__main__':
+    main()
